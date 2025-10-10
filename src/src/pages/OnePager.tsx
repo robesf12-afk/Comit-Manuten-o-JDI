62a515c7faa78281d56442e7b8d8ce3838962231
robesf12-afk <robesf12@gmail.com>
2025-10-10 18:37:42 -0300

    Delete src/src directory

--- src/src/pages/OnePager.tsx
+++
@@ -1,8 +0,0 @@
-export default function OnePager() {
-  return (
-    <div style={{ fontFamily: "system-ui, Arial", padding: 24 }}>
-      <h2>One-Pager OKR</h2>
-      <p>Resumo dos objetivos, metas, KPIs e status.</p>
-    </div>
-  );
-}
--- src/src/pages/Treinamentos.tsx
+++
@@ -1,9 +0,0 @@
-export default function Treinamentos() {
-  return (
-    <div style={{ fontFamily: "system-ui, Arial", padding: 24 }}>
-      <h2>Treinamentos de Manutenção</h2>
-      <p>Cronograma, materiais, presença, avaliações…</p>
-    </div>
-  );
-}
-src/page
