ea5290c530adec06fe4bc4070c91cb88a919a9f2
robesf12-afk <robesf12@gmail.com>
2025-10-10 18:20:36 -0300

    Update App.tsx

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,8 +1,1 @@
-export default function App() {
-  return (
-    <div style={{fontFamily:'Arial', padding:16}}>
-      <h1>Comitê de Manutenção — está no ar! ✅</h1>
-      <p>Se você está vendo esta página, o deploy deu certo.</p>
-    </div>
-  )
-}
+
