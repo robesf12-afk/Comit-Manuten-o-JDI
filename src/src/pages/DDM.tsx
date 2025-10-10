e092debf8b542156e788203222753d7249b5b6af
robesf12-afk <robesf12@gmail.com>
2025-10-10 11:43:48 -0300

    Create DDM.tsx

---
+++ src/src/pages/DDM.tsx
@@ -0,0 +1,8 @@
+export default function DDM() {
+  return (
+    <div style={{ fontFamily: "system-ui, Arial", padding: 24 }}>
+      <h2>DDM – Diálogo de Manutenção</h2>
+      <p>Aqui vai o conteúdo (links, formulários, anexos, checklist…).</p>
+    </div>
+  );
+}
