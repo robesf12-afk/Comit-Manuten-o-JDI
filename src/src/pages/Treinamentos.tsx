fd63c49afbc0fc027ee37635e810968ce9f53819
robesf12-afk <robesf12@gmail.com>
2025-10-10 11:44:20 -0300

    Create Treinamentos.tsx

---
+++ src/src/pages/Treinamentos.tsx
@@ -0,0 +1,9 @@
+export default function Treinamentos() {
+  return (
+    <div style={{ fontFamily: "system-ui, Arial", padding: 24 }}>
+      <h2>Treinamentos de Manutenção</h2>
+      <p>Cronograma, materiais, presença, avaliações…</p>
+    </div>
+  );
+}
+src/page
