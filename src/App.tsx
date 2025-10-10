eae989f46287d0b47abfbb8d5ab2ea3c6d9a6446
robesf12-afk <robesf12@gmail.com>
2025-10-10 11:30:35 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "system-ui, Arial", padding: 24 }}>
+      <h1>Comitê de Manutenção • JDI</h1>
+      <p>App online! 🚀</p>
+    </div>
+  );
+}
