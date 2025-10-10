71626fec7f12ca360001b3cc25a6633b7ffc0813
robesf12-afk <robesf12@gmail.com>
2025-10-10 10:40:59 -0300

    Create App.tsx

---
+++ src/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{fontFamily:'Arial', padding:16}}>
+      <h1>Comitê de Manutenção — está no ar! ✅</h1>
+      <p>Se você está vendo esta página, o deploy deu certo.</p>
+    </div>
+  )
+}
