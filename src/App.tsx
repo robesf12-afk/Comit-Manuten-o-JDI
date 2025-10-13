4480fd429cbb0c6ca1d5ea7f5e3ddb32c0005c96
robesf12-afk <robesf12@gmail.com>
2025-10-13 19:11:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ export default function App() {
     { t: "One Pager", d: "Materiais de uma página", href: "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/roberta_dossantos_kof_com_mx/Documents/ONE%20PAGER?csf=1&web=1&e=8wmcwI" },
     { t: "Papéis & Responsabilidades", d: "Definições de responsabilidades", href: "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/roberta_dossantos_kof_com_mx/Documents/PAP%C3%89IS%20E%20RESPONSABILIDADES?csf=1&web=1&e=sFPbdV" },
     { t: "Treinamentos", d: "Materiais e trilhas", href: "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/roberta_dossantos_kof_com_mx/Documents/TREINAMENTOS?csf=1&web=1&e=XxZGMO" },
-    { t: "Checklist DDM’s (Forms)", d: "Abrir formulário com prefill", href: "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=QtWUcBU4gkyx1WkX0EQ89IvsP_YVPjJJhA-rzC2o4A5UNFNCSkE3RjZON1M3NlFQWTgyTkc0MUFSWC4u&topview=Prefill" },
-    { t: "Checklist (Forms)", d: "Abrir formulário com prefill", href: "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=QtWUcBU4gkyx1WkX0EQ89IvsP_YVPjJJhA-rzC2o4A5UNlJNM01FRTFFQ0NVUUVKMjdYRVdWUDNUUC4u&topview=Prefill" },
+    { t: "Checklist Pós-Partida CIP/SETUP/PCM/Grandes Manutenções (Forms)", d: "Abrir formulário com prefill", href: "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=QtWUcBU4gkyx1WkX0EQ89IvsP_YVPjJJhA-rzC2o4A5UNFNCSkE3RjZON1M3NlFQWTgyTkc0MUFSWC4u&topview=Prefill" },
+    { t: "REGISTRO DE REUNIÕES DE ABERTURA DE PCM E PRESTAÇÃO DE CONTAS (Forms)", d: "Abrir formulário com prefill", href: "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage&subpage=design&id=QtWUcBU4gkyx1WkX0EQ89IvsP_YVPjJJhA-rzC2o4A5UNlJNM01FRTFFQ0NVUUVKMjdYRVdWUDNUUC4u&topview=Prefill" },
   ];
 
   return (
