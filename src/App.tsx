c5a1a225bfbf79061f5385555619b5a43bebe97b
robesf12-afk <robesf12@gmail.com>
2025-10-13 20:42:23 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
+// ...imports (se houver)
+
 export default function App() {
-  // ajuste os links/descrições aqui
-  const links: { t: string; d: string; href: string }[] = [
-    { t: "DDM’s", d: "Documentos do DDM", href: "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/roberta_dossantos_kof_com_mx/Documents/DDM%20?csf=1&web=1" },
-    { t: "Fechamentos (OKR)", d: "Pasta de Fechamentos", href: "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/roberta_dossantos_kof_com_mx/Documents/FECHAMENTOS" },
-    { t: "Informativos", d: "Comunicados e materiais", href: "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/roberta_dossantos_kof_com_mx/Documents/INFORMATIVOS" },
-    { t: "One Pager", d: "Materiais de uma página", href: "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/roberta_dossantos_kof_com_mx/Documents/ONE%20PAGER" },
-    { t: "Papéis & Responsabilidades", d: "Definições de responsabilidades", href: "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/roberta_dossantos_kof_com_mx/Documents/PAPEIS%20RESPONSABILIDADES" },
-    { t: "Treinamentos", d: "Materiais e trilhas", href: "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/roberta_dossantos_kof_com_mx/Documents/TREINAMENTOS" },
-    // Checklists (Forms)
-    { t: "Checklist Pós-Partida", d: "CIP/SETUP/PCM/Grandes Manutenções", href: "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage..." },
-    { t: "Registro de Abertura de PCM", d: "Abertura de PCM e Prestação de Contas", href: "https://forms.office.com/Pages/DesignPageV2.aspx?origin=NeoPortalPage..." }
-  ];
+  // seu array de links continua igual…
 
   return (
-    <div className="container">
+    <>
+      {/* APP BAR */}
+      <header className="appbar">
+        <div className="appbar-side">
+          <img src="/logo-femsa.png" alt="FEMSA" className="logo-femsa" />
+        </div>
+
+        <h1 className="appbar-title">
+          Comitê de Manutenção • JDI
+        </h1>
+
+        <div className="appbar-side">
+          {/* use a menor versão redonda (180/192) do comitê */}
+          <img src="/logo-comite-180.png" alt="Comitê" className="logo-comite" />
+        </div>
+      </header>
+
+      {/* o restante do seu layout permanece */}
+      <main className="container">
+        {/* …suas cards/links… */}
+      </main>
+
+      {/* …footer se tiver… */}
+    </>
+  );
+}
+
      <header className="topbar">
   <div className="brand">
     <img
