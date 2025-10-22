367c7a69659e5e135c6920bc3ab0d1bf1ceabdb7
robesf12-afk <robesf12@gmail.com>
2025-10-21 22:33:59 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // <= ESSENCIAL
+import { initPush } from "./push";
+
+initPush();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
