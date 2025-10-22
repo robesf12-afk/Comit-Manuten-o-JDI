8d990985fa071201cc0c87c4cf4304d6b707c2d0
robesf12-afk <robesf12@gmail.com>
2025-10-21 22:32:35 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,25 @@
-// public/sw.js
-const CACHE_VERSION = 'v10';
-
-self.addEventListener('install', () => {
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
 });
-
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
+const CACHE = 'app-cache-v1';
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then((res) =>
+      res || fetch(req).then((net) => {
+        caches.open(CACHE).then((c) => c.put(req, net.clone()));
+        return net;
+      })
+    )
+  );
+});
+
+// Carrega o worker do OneSignal também por aqui (não conflita)
+try {
+  importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+} catch (_) {}
