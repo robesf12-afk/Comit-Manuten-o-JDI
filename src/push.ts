3567efa558677ac0712e4984f2f7411894eff383
robesf12-afk <robesf12@gmail.com>
2025-10-21 22:46:19 -0300

    Update push.ts

--- src/push.ts
+++ src/push.ts
@@ -9,19 +9,18 @@ const APP_ID = "c9dee04e-1964-428a-90ec-f38dbe8c9be3"; // seu App ID
 
 export function initPush() {
   window.OneSignalDeferred = window.OneSignalDeferred || [];
-
-  // Marca quando o SDK efetivamente processar a fila
   window.OneSignalDeferred.push(async function (OneSignal: any) {
     window.__OS_READY__ = true;
-
     await OneSignal.init({
       appId: APP_ID,
       allowLocalhostAsSecureOrigin: true,
       serviceWorkerPath: "/OneSignalSDKWorker.js",
       serviceWorkerUpdaterPath: "/OneSignalSDKUpdaterWorker.js",
       serviceWorkerParam: { scope: "/" },
-      notifyButton: { enable: true },
-      promptOptions: { slidedown: { enabled: false, autoPrompt: false } } // vamos chamar manualmente
+
+      // UI do OneSignal pode ficar oculta na apresentação
+      notifyButton: { enable: false },
+      promptOptions: { slidedown: { enabled: false, autoPrompt: false } }
     });
   });
 }
