7b48e69ce0256a27683a6acaefd2ee00a4104949
robesf12-afk <robesf12@gmail.com>
2025-10-25 21:15:14 -0300

    Update OneSignalSDKWorker.js

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,5 +1,3 @@
-// hotfix: worker neutro só para destravar o domínio
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
-
 
