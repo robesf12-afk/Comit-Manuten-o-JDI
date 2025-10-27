e1ae70604c4682d351234656b432b78b07a24bba
robesf12-afk <robesf12@gmail.com>
2025-10-26 23:33:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,97 +12,35 @@ import {
   IconReconhecimentos,
 } from "./icons";
 
-/* ----------------------------- MENU (Drawer) ----------------------------- */
-const MENU = [
-  {
-    id: "registro",
-    title: "Registro de Reuniões / Prestação de Contas",
-    href: "#registro",
-    Icon: IconRegistroPCM,
-  },
-  {
-    id: "checklist",
-    title: "Checklist Pós-Partida",
-    href: "#checklist",
-    Icon: IconChecklist,
-  },
-  {
-    id: "ddms",
-    title: "DDM’s",
-    href: "#ddms",
-    Icon: IconDDM,
-  },
-  {
-    id: "programacao",
-    title: "Programação de PCM",
-    href: "#programacao",
-    Icon: IconChecklist,
-  },
-  {
-    id: "distribuicao",
-    title: "Painel de Distribuição de Horas",
-    href: "#distribuicao",
-    Icon: IconOKR,
-  },
-  {
-    id: "okr",
-    title: "OKR de Manutenção",
-    href: "#okr",
-    Icon: IconOKR,
-  },
-  {
-    id: "informativos",
-    title: "Informativos",
-    href: "#informativos",
-    Icon: IconInfo,
-  },
-  {
-    id: "papeis",
-    title: "Papéis & Responsabilidades",
-    href: "#papeis",
-    Icon: IconPapeis,
-  },
-  {
-    id: "onepager",
-    title: "One Pager",
-    href: "#onepager",
-    Icon: IconOnePager,
-  },
-  {
-    id: "treinamentos",
-    title: "Treinamentos",
-    href: "#treinamentos",
-    Icon: IconTreinamentos,
-  },
-  {
-    id: "reconhecimentos",
-    title: "Reconhecimentos",
-    href: "#reconhecimentos",
-    Icon: IconReconhecimentos,
-  },
-];
+/* ---------------- URLs curtas (quebradas em partes) ---------------- */
+const U = {
+  registro: "https://forms.office.com/r/" + "mt0JTBJiK6" + "?origin=lprLink",
+  checklist: "https://forms.office.com/r/" + "XM1hQ5YCrp" + "?origin=lprLink",
+  ddms:
+    "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/" +
+    "roberta_dossantos_kof_com_mx/Documents/DDM%C2%B4S" +
+    "?csf=1&web=1&e=kXfLLD",
+  programacao:
+    "https://cocacolafemsa.sharepoint.com/:f:/r/sites/" +
+    "PROGRAMAOPREPCMJUNDIAIOSASCO/Documentos%20Compartilhados/" +
+    "PAINEL%20DISTRIBUI%C3%87%C3%83O%20DE%20HORAS" +
+    "?csf=1&web=1&e=Ye4Wad",
+  distribuicao:
+    "https://cocacolafemsa.sharepoint.com/:f:/r/sites/" +
+    "PROGRAMAOPREPCMJUNDIAIOSASCO/Documentos%20Compartilhados/" +
+    "PROGRAMA%C3%87%C3%83O%20PRE%20PCM" +
+    "?csf=1&web=1&e=LYYchz",
+  okr:
+    "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/" +
+    "roberta_dossantos_kof_com_mx/Documents/FECHAMENTOS" +
+    "?csf=1&web=1&e=e0QIRb",
+  informativos:
+    "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/" +
+    "roberta_dossantos_kof_com_mx/Documents/INFORMATIVOS" +
+    "?csf=1&web=1&e=dy3e4Y",
+  papeis:
+    "https://cocacolafemsa-my.sharepoint.com/:f:/r/personal/" +
+    "roberta_dossantos_kof_com_mx/Documents/" +
+    "PAP%C3%89IS%20E%20RESPONSABILIDADES" +
+    "?c
 
-/* --------------------------- CARDS (Conteúdo) ---------------------------- */
-type Card = {
-  id: string;
-  title: string;
-  subtitle: string;
-  href: string;
-  Icon: React.ComponentType<any>;
-};
-
-const CARDS: Card[] = [
-  {
-    id: "registro",
-    title:
-      "REGISTRO DE REUNIÕES DE ABERTURA DE PCM E PRESTAÇÃO DE CONTAS",
-    subtitle: "Aberturas de PCM e Prestação de Contas",
-    href: "https://forms.office.com/r/mt0JTBJiK6?origin=lprLink",
-    Icon: IconRegistroPCM,
-  },
-  {
-    id: "checklist",
-    title: "CHECKLIST PÓS-PARTIDA",
-    subtitle: "CIP/SETUP/PCM/Grandes Manutenções",
-    href: "https://forms.office.com/r/XM1hQ5YCrp?origin=lprLink",
-    Icon: IconCh
